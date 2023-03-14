413e778ab1e70f9fa4faa009262a916826e9a3b4
Nick Sinnikov <nickosinnikov@outlook.com>
2023-03-14 17:41:28 +0300

    time filter hidden

--- src/components/Aside/TimeFilter.jsx
+++ src/components/Aside/TimeFilter.jsx
@@ -1,37 +1,34 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import InputRange from 'react-input-range';
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+//import InputRange from 'react-input-range';
 
-export default function TimeFilter(){
-    const [isHidden, setHidden] = useState({
-        departure: true,
-        arrival: true,
-     });
-     const onHidden = (name) => {
-        setHidden((prev) => ({ ...prev, [name]: !prev[name] }));
-     };
-    return(
-        <div className="time-filter">
-            <div className="timeFilter_header">
-               <h4 className="timeFilter_header-title title title-goThere">
-                  Туда
-               </h4>
-               <button
-                  type="button"
-                  className={`timeFilter_button ${
-                     isHidden.departure ? 'active-button' : 'inactive-button'
-                  }`}
-                  onClick={() => onHidden('departure')}
-               />
-            </div>
-            <div
-               className={`timeFilter_form ${
-                  isHidden.departure ? 'hidden' : ''
-               }`}
-            >
-               <p className="timeFilter_title">Время отправления</p>
-               <div className="timeFilter_range">
-                  <InputRange
+export default function TimeFilter() {
+  const [isHidden, setHidden] = useState({
+    departure: true,
+    arrival: true,
+  });
+  const onHidden = (name) => {
+    setHidden((prev) => ({ ...prev, [name]: !prev[name] }));
+  };
+  return (
+    <div className="aside-item">
+      <div className="time-filter__block">
+        <div className="time-filter_header">
+          <h4 className="time-filter_header-title title title-goThere">Туда</h4>
+          <button
+            type="button"
+            className={`time-filter_button ${
+              isHidden.departure ? "active-button" : "inactive-button"
+            }`}
+            onClick={() => onHidden("departure")}
+          />
+        </div>
+        <div
+          className={`time-filter_form ${isHidden.departure ? "hidden" : ""}`}
+        >
+          <p className="time-filter_title">Время отправления</p>
+          <div className="time-filter_range">
+            {/* <InputRange
                      formatLabel={(value) => `${value}:00`}
                      minValue={0}
                      maxValue={24}
@@ -42,13 +39,11 @@ export default function TimeFilter(){
                     //  onChange={(value) =>
                     //     handleChange('start_departure', value)
                     //  }
-                  />
-               </div>
-               <p className="timeFilter_title title title-right">
-                  Время прибытия
-               </p>
-               <div className="timeFilter_range">
-                  <InputRange
+                  /> */}
+          </div>
+          <p className="time-filter_title title title-right">Время прибытия</p>
+          <div className="time-filter_range">
+            {/* <InputRange
                      formatLabel={(value) => `${value}:00`}
                      minValue={0}
                      maxValue={24}
@@ -57,9 +52,55 @@ export default function TimeFilter(){
                     //     max: start_arrival_hour_to,
                     //  }}
                     //  onChange={(value) => handleChange('start_arrival', value)}
-                  />
-               </div>
-            </div>
+                  /> */}
+          </div>
         </div>
-    )
+      </div>
+      <div className="time-filter__block">
+        <div className="time-filter_header">
+          <h4 className="time-filter_header-title title title-goThere">
+            Обратно
+          </h4>
+          <button
+            type="button"
+            className={`time-filter_button ${
+              isHidden.arrival ? "active-button" : "inactive-button"
+            }`}
+            onClick={() => onHidden("arrival")}
+          >
+            <p>{isHidden.arrival ? "+" : "-"}</p> </button>
+        </div>
+        <div className={`time-filter_form ${isHidden.arrival ? "hidden" : ""}`}>
+          <p className="time-filter_title">Время отправления</p>
+          <div className="time-filter_range">
+            {/* <InputRange
+                     formatLabel={(value) => `${value}:00`}
+                     minValue={0}
+                     maxValue={24}
+                    //  value={{
+                    //     min: start_departure_hour_from,
+                    //     max: start_departure_hour_to,
+                    //  }}
+                    //  onChange={(value) =>
+                    //     handleChange('start_departure', value)
+                    //  }
+                  /> */}
+          </div>
+          <p className="time-filter_title title title-right">Время прибытия</p>
+          <div className="time-filter_range">
+            {/* <InputRange
+                     formatLabel={(value) => `${value}:00`}
+                     minValue={0}
+                     maxValue={24}
+                    //  value={{
+                    //     min: start_arrival_hour_from,
+                    //     max: start_arrival_hour_to,
+                    //  }}
+                    //  onChange={(value) => handleChange('start_arrival', value)}
+                  /> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
